ce31404694378ea4ff5fce832b4bc6bb7cf248f5
dhirajrathod <dhirajrathod3000@gmail.com>
2021-08-09 09:12:39 +0530

    ADDED: Ability to add hotel

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,14 @@
+public class Hotel {
+    String hotelName;
+    int ratesForRegularCustomer;
+
+    /**
+     * Constructor For Hotel
+     * @param hotelName
+     * @param ratesForRegularCustomer
+     */
+    public Hotel(String hotelName, int ratesForRegularCustomer) {
+        this.hotelName = hotelName;
+        this.ratesForRegularCustomer = ratesForRegularCustomer;
+    }
+}
--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelReservation {
-    public static void main(String[] args){
-        System.out.println("Welcome To Hotel Reservation System");
+
+    private List<Hotel> hotelList;
+
+    /**
+     * con
+     * @param hotelList
+     */
+    public HotelReservation(List<Hotel> hotelList) {
+        this.hotelList = new ArrayList<>();
+    }
+
+    /**
+     * add Hotel Method
+     * @param hotel
+     */
+    public void add(Hotel hotel) {
+        this.hotelList.add(hotel);
     }
 }
